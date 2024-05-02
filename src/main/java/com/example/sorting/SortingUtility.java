a17c79ae74d3fe990c87d746ad8784615a071064
Khanhl888 <kha2130096@maricopa.edu>
2024-05-02 14:49:03 -0700

    project

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -6,18 +6,57 @@ public class SortingUtility {
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
 
         // TODO implement Gnome Sort here
-    }
+        int index = 0;
+        while (index < data.length) {
+            if (index == 0 || data[index].compareTo(data[index - 1]) >= 0) {
+                index++;
+            } else {
+                swap(data, index, index - 1);
+                index--;
+            }
+        }
+
+}
 
 
     public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
-
         // TODO implement Cocktail Shaker Sort here
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < data.length - 1; i++) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
+                    swap(data, i, i + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+            swapped = false;
+            for (int i = data.length - 2; i >= 0; i--) {
+                if (data[i].compareTo(data[i + 1]) > 0) {
+                    swap(data, i, i + 1);
+                    swapped = true;
+                }
+            }
+        } while (swapped);
     }
 
 
     public static <T extends Comparable<T>> void shellSort(T[] data) {
 
         // TODO implement Shell Sort here
+        int n = data.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                T temp = data[i];
+                int j;
+                for (j = i; j >= gap && data[j - gap].compareTo(temp) > 0; j -= gap) {
+                    data[j] = data[j - gap];
+                }
+                data[j] = temp;
+            }
+        }
+
     }
 
     private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2) {
